249814dde3f9c8becb14c92e9ab99878f14988c9
Nasir Lawal <nasirlawal001@gmail.com>
2021-02-27 22:44:25 +0100

    lifecircle hooks

--- src/Views/Races.vue
+++ src/Views/Races.vue
@@ -5,9 +5,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 import { RaceModel } from '@/models/RaceModel';
 import Race from '@/components/Race.vue';
+import { useRaceService } from '@/composables/RaceService';
 
 export default defineComponent({
   name: 'Races',
@@ -16,32 +17,12 @@ export default defineComponent({
   },
 
   setup() {
-    const races = ref<Array<RaceModel>>([
-      {
-        id: 12,
-        name: 'Paris',
-        ponies: [
-          { id: 1, name: 'Gentle Pie', color: 'YELLOW' },
-          { id: 2, name: 'Big Soda', color: 'ORANGE' },
-          { id: 3, name: 'Gentle Bottle', color: 'PURPLE' },
-          { id: 4, name: 'Superb Whiskey', color: 'GREEN' },
-          { id: 5, name: 'Fast Rainbow', color: 'BLUE' }
-        ],
-        startInstant: '2020-02-18T08:02:00Z'
-      },
-      {
-        id: 13,
-        name: 'Tokyo',
-        ponies: [
-          { id: 6, name: 'Fast Rainbow', color: 'BLUE' },
-          { id: 7, name: 'Gentle Castle', color: 'GREEN' },
-          { id: 8, name: 'Awesome Rock', color: 'PURPLE' },
-          { id: 9, name: 'Little Rainbow', color: 'YELLOW' },
-          { id: 10, name: 'Great Soda', color: 'ORANGE' }
-        ],
-        startInstant: '2020-02-18T08:03:00Z'
-      }
-    ]);
+    const race = useRaceService();
+    const races = ref<Array<RaceModel | null>>();
+
+    onMounted(async () => {
+      races.value = await race.list();
+    });
 
     return { races };
   }
--- tests/unit/views/Races.spec.ts
+++ tests/unit/views/Races.spec.ts
@@ -1,9 +1,21 @@
 import { flushPromises, mount } from '@vue/test-utils';
 import Races from '@/views/Races.vue';
 import Race from '@/components/Race.vue';
+import { RaceModel } from '@/models/RaceModel';
+
+const mockRaceService = {
+  list: jest.fn()
+};
+jest.mock('@/composables/RaceService', () => ({
+  useRaceService: () => mockRaceService
+}));
 
 describe('Races.vue', () => {
   test('should display every race name in a title', async () => {
+    mockRaceService.list.mockReturnValue([
+      { name: 'London', startInstant: '2020-02-18T08:02:00Z' },
+      { name: 'New York', startInstant: '2020-02-18T08:03:00Z' }
+    ] as Array<RaceModel>);
     const wrapper = mount(Races);
     await flushPromises();
 
@@ -12,8 +24,8 @@ describe('Races.vue', () => {
     expect(racesField).not.toBeUndefined();
     // You need to have a field `races` initialized with 2 races
     expect(racesField).toHaveLength(2);
-    expect(racesField[0].name).toBe('Paris');
-    expect(racesField[1].name).toBe('Tokyo');
+    expect(racesField[0].name).toBe('London');
+    expect(racesField[1].name).toBe('New York');
 
     const raceComponents = wrapper.findAllComponents(Race);
     // You should have a `Race` component per race in your template
